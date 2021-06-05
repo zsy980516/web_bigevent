c36d8796387a3979f1bb0db930c0cb470d0be81b
zsy <2935643340@qq.com>
2021-06-05 19:37:26 +0800

    完成个人中心功能的开发

---
+++ code/assets/js/user_info.js
@@ -0,0 +1,51 @@
+$(function() {
+    var form = layui.form;
+    form.verify({
+        nickname: function(value) {
+            if (value.length > 6) {
+                return '昵称必须在1-6个字符之间';
+            }
+        }
+    })
+    initUserInfo();
+
+
+    //// 初始化用户的基本信息
+    function initUserInfo() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/userinfo',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.measseg);
+                }
+                console.log(res)
+                    // 调用 `form.val()`
+                    // 方法为表单赋值：          
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+    // // 重置表单的数据
+    $('#btnReset').on('click', function(e) {
+            e.preventDefault()
+            initUserInfo();
+        })
+        //监听表单的提交事件
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg(res.message);
+                // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+                window.parent.getUserInfo()
+            }
+        })
+    })
+})
---
+++ code/assets/js/user_pwd.js
@@ -0,0 +1,42 @@
+$(function() {
+    var form = layui.form;
+
+    //自定义校验规则
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        samePwd: function(value) {
+            if (value === $('[name=oldPwd]').val()) {
+                return '新旧密码不能相同！'
+            }
+        },
+        rePwd: function(value) {
+            if (value !== $('[name=newPwd]').val()) {
+                return '两次密码不一致'
+            }
+        }
+    })
+
+    //重置密码
+    $('.layui-form').on('submit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/updatepwd',
+            data: $(this).serialize(),
+            success: function(res) {
+                console.log(res);
+
+                if (res.status !== 0) {
+                    return layui.layer.msg(res.message)
+                }
+                layui.layer.msg(res.message)
+                    // 重置表单
+                $('.layui-form')[0].reset()
+            }
+
+
+        })
+    })
+})
